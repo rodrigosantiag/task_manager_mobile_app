bd4c36dc4c6700d5767792f62b573c3b90fe2820
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-04-22 22:19:32 -0300

    Saving all

--- src/main.ts
+++ src/main.ts
@@ -10,4 +10,4 @@ import { AppModule } from "./app/app.module";
 // A NativeScript Angular app needs to make both paradigms work together,
 // so we provide a wrapper platform object, platformNativeScriptDynamic,
 // that sets up a NativeScript application and can bootstrap the Angular framework.
-platformNativeScriptDynamic().bootstrapModule(AppModule);
+platformNativeScriptDynamic({createFrameOnBootstrap: true}).bootstrapModule(AppModule);
