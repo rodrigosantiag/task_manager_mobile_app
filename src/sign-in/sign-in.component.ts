6f7872c638f69b8390be641837ec60b0a8ac4721
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-04-30 05:04:19 -0300

    Implement signInUser

--- src/sign-in/sign-in.component.ts
+++ src/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
-import {Component} from "@angular/core";
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+
+import {AuthService} from '../shared/auth.service';
 
 @Component({
     selector: 'app-sign-in',
@@ -8,5 +12,41 @@ import {Component} from "@angular/core";
 })
 
 export class SignInComponent {
-    
+  public form: FormGroup;
+  public formErrors: Array<string>;
+  public submitted: boolean;
+
+  public constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router) {
+    this.setupForm();
+    this.submitted = false;
+    this.formErrors = null;
+  }
+
+  public signInUser() {
+    this.submitted = true;
+    this.authService.signIn(this.form.get('email').value, this.form.get('password').value)
+      .subscribe(
+        () => {
+          this.router.navigate(['/home']);
+          this.formErrors = null;
+        },
+        error => {
+          if (error.status === 401) {
+            this.formErrors = error._body.errors;
+          }else {
+            this.formErrors = ['Não possível processar a sua solicitação. Por favor, tente mais tarde.'];
+          }
+          this.submitted = false;
+        }
+      );
+  }
+
+  private setupForm() {
+    this.form = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required]
+    });
+  }
 }
+
+
