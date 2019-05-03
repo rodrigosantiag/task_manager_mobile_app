52b60b69a2307fc12e3dbf22c03c79783b3811ae
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-05-03 05:05:43 -0300

    Create DateTimePickerModal Component base

---
+++ src/datetime-picker-modal/datetime-picker-modal.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from "@angular/core";
+import {ModalDialogParams} from "nativescript-angular";
+import {Page} from "tns-core-modules/ui/page";
+import{DatePicker} from "tns-core-modules/ui/date-picker";
+import {TimePicker} from "tns-core-modules/ui/time-picker";
+
+@Component({
+  selector: 'app-datetime-picker-modal',
+  moduleId: module.id,
+  templateUrl: './datetime-picker-modal.component.html'
+})
+
+export class DatetimePickerModalComponent implements OnInit{
+  public preSelectedDateTime: Date;
+  public datePicker: DatePicker;
+  public timePicker: TimePicker;
+
+  public constructor(private modalParams: ModalDialogParams, private page: Page) {}
+
+
+  public ngOnInit(): void {
+  }
+
+}
